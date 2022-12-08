b929a0ac93ce983cd31d3b80872c0edfd4527646
Robbe De Proft <rdeproft12@gmail.com>
2022-12-08 17:45:06 +0100

    Day 6

---
+++ aoc06/task2.py
@@ -0,0 +1,14 @@
+def main(filename):
+    with open(filename) as file:
+        data = file.readline()
+        print(get_marker_position(data, 14))
+
+
+def get_marker_position(data, length):
+    for i in range(len(data) - length + 1):
+        if len(set(data[i:i+length])) == length:
+            return i + length
+
+
+if __name__ == '__main__':
+    main('input.txt')
